ccf634516bd9b926c8e81acb1deade2429f2b3ce
Kun Huang <kkun.huang@gmail.com>
2018-10-09 21:57:29 -0400

    First commit

---
+++ twitter_alamofire_demo/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  twitter_alamofire_demo
+//
+//  Created by Kun Huang on 10/9/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+    var name: String?
+    
+    init(dictionary: [String : Any]) {
+        super.init()
+    }
+    
+}
