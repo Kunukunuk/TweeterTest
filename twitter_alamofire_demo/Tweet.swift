979f48378df54a546e84846c5501730ff80c3959
Kun Huang <kkun.huang@gmail.com>
2018-10-12 21:42:16 -0400

    data  modal

--- twitter_alamofire_demo/Tweet.swift
+++ twitter_alamofire_demo/Tweet.swift
@@ -24,6 +24,47 @@ class Tweet: NSObject {
     
     init(dictionary: [String : Any]) {
         super.init()
+        
+        var dictionary = dictionary
+        
+        // Is this a re-tweet?
+        if let originalTweet = dictionary["retweeted_status"] as? [String: Any] {
+            let userDictionary = dictionary["user"] as! [String: Any]
+            self.retweetedByUser = User(dictionary: userDictionary)
+            
+            // Change tweet to original tweet
+            dictionary = originalTweet
+        }
+        
+        id = dictionary["id"] as? Int
+        text = dictionary["text"] as? String
+        favoriteCount = dictionary["favorite_count"] as? Int
+        favorited = dictionary["favorited"] as? Bool
+        retweetCount = dictionary["retweet_count"] as! Int
+        retweeted = dictionary["retweeted"] as! Bool
+        
+        // TODO: initialize user
+        let user = dictionary["user"] as! [String: Any]
+        self.user = User(dictionary: user)
+        
+        // TODO: Format and set createdAtString
+        // Format createdAt date string
+        let createdAtOriginalString = dictionary["created_at"] as! String
+        let formatter = DateFormatter()
+        // Configure the input format to parse the date string
+        formatter.dateFormat = "E MMM d HH:mm:ss Z y"
+        // Convert String to Date
+        let date = formatter.date(from: createdAtOriginalString)!
+        // Configure output format
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        // Convert Date to String and set the createdAtString property
+        createdAtString = formatter.string(from: date)
     }
     
+    static func tweets(with array: [[String: Any]]) -> [Tweet] {
+        return array.flatMap({ (dictionary) -> Tweet in
+            Tweet(dictionary: dictionary)
+        })
+    }
 }
