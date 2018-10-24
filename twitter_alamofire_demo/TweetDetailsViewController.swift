0c9b91cbc81d6920ab5d409474049b5836792886
Kun Huang <kkun.huang@gmail.com>
2018-10-23 23:33:29 -0400

    set up detail view

---
+++ twitter_alamofire_demo/TweetDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TweetDetailsViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Kun Huang on 10/23/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class TweetDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
